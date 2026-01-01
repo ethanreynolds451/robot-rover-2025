3331adff8848c25b166bb35c67dc559085482b2a
ethanreynolds <ethanreynolds451@gmail.com>
2026-01-01 13:37:36 -0500

    Adjusted library include

---
+++ code/sensor/libraries/libraries.h
@@ -0,0 +1,31 @@
+#ifndef LIBRARIES_h
+#define LIBRARIES_h
+
+// These libraries are included by default in Arduino instalation
+#include <Arduino.h>
+#include <SoftwareSerial.h>                                     //Virtual serial port
+#include <SPI.h>                                                //Serial peripheral interface
+#include <Wire.h>                                               //Communication
+
+// Need to install Adafruit IO Bus and Adafruit Sensor from Arduino library manager
+// they are directly referenced by other libraries and can not be included directly from folder without difficulty
+
+// These libraries are downloaded from the internet and placed in a "libraries" folder in the same directory as the program
+#include "HCSR04_ultrasonic_sensor/src/HCSR04.h"      //Ultrasonic interface
+#include "Adafruit_MPU6050/Adafruit_MPU6050.h"        //Gyroscope interface
+#include "TinyGPSPlus-master/src/TinyGPSPlus.h"           //GPS interface
+#include "TinyGPSPlus-master/src/TinyGPS++.h"           //GPS interface
+#include "QMC5883LCompass-1.2.3/src/QMC5883LCompass.h"  //Compass interface
+#include "IRremote-4.4.1/src/IRremote.h"                // IR remote digital
+#include "VL53L0X-1.3.1/VL53L0X.h"                      //LOF sensors
+#include "PCF8575-master/PCF8575.h"                     //PCF port expander 16 bit
+  
+// Need to include cpps for some reason because it will not work otherwise
+#include "HCSR04_ultrasonic_sensor/src/HCSR04.cpp"      //Ultrasonic interface
+#include "Adafruit_MPU6050/Adafruit_MPU6050.cpp"  //Gyroscope interface
+#include "TinyGPSPlus-master/src/TinyGPS++.cpp"           //GPS interface
+#include "QMC5883LCompass-1.2.3/src/QMC5883LCompass.cpp"  //Compass interface
+#include "VL53L0X-1.3.1/VL53L0X.cpp"                      //LOF sensors
+#include "PCF8575-master/PCF8575.cpp"                     //PCF port expander 16 bit
+
+#endif
