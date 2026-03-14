8b57ab0853f740c9ce3052a6623330c884828df9
Ethan Reynolds <ethanreynolds@ERMacAir.local>
2026-03-14 17:25:51 -0400

    Refactoring sensor structure

---
+++ code/sensorBoard/libraries/libraries.h
@@ -0,0 +1,9 @@
+#ifndef LIBRARIES_h
+#define LIBRARIES_h
+
+//#include "arduino/Arduino.h"
+
+#include "I2CDisplayController/src/i2cdisplaycontroller.h"
+#include "I2CDisplayController/src/i2cdisplaycontroller.cpp"
+
+#endif
