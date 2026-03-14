98b862120cb06de73eb3882324ac4589398bcdd9
Ethan Reynolds <ethanreynolds@ERMacAir.local>
2026-03-14 17:32:43 -0400

    Upload changes

--- code/controlBoard/vehicle/vehicleCommunications/vehicleSerial/vehicleSerial.ino
+++ code/controlBoard/vehicle/vehicleCommunications/vehicleSerial/vehicleSerial.ino
@@ -2,30 +2,13 @@
 
 #include "vehicleSerial.h"
 
-RobotSerial computer(115200);
+RobotSerial computer(115200, 64);
 
 void setup(){
     computer.begin();
-    computer.add("Hello ");
-    computer.add("World!\n");
-    computer.add("This is a really long string of text that will not fit in a single 64 byte packet. It is being added to test functionality of the packet management feature.\n");
-    if(computer.add("This text will overflow the buffer. It is being added to verify that the safeguards to handle buffer overflow are in place and working properly. asfhqiluwehriluwahflkuahweslukfghawelsujgfluqweagfahjklfdasbhjsdfgbfdjklassdbfjklsdhajkflhewaskljdfghklweajsgflkjashdfeilqwhflukqjweghaqgfliueghsaklfjhweqailsuekfhwlqeiufghewlqihstoiuqewhfiuqwelhjfipmoqewhrdcoaiuwhlrcawihemrsñjqmoiwepahfiqlwñaefjdpoiqwñelahdjljrqlwekhjasfiopañljsfpmilqwjeciorlmhwerp,fqñjierxpqolñihrejpiocmaopwmrhAIPWMEFPIOAJEIMFHWA,EIUWHMF")){
-      Serial.println("This shouldn't be happening"); 
-    } else {
-      Serial.println("A packet wasn't added because it was too long"); 
-    }
-    delay(1000); 
 }
 
 void loop(){
-    computer.update();
-    if(computer.available()){
-      // Serial.println(computer.available());
-        // Degug why isn't this zero?
-      // Serial.println(computer.get_buffer());
-        // Debug: is there actually anyting in there?
-      Serial.print("Received: ");
-      Serial.println(computer.get());
-    }
-    delay(1000); 
+    computer.write("Hello World!\n");
+    delay(1000);
 }
