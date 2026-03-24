ceeeae3f34d1bbf9df163cffe0f81cb5ca23bb22
= <=>
2026-03-24 12:45:10 -0400

    Added all sensor board hardware directories

--- code/sensorBoard/vehicle/vehicleSensors/vehicleSensors.h
+++ code/sensorBoard/vehicle/vehicleSensors/vehicleSensors.h
@@ -1,7 +1,11 @@
 #ifndef VEHICLESENSORS_h
 #define VEHICLESENSORS_h
 
+// Comment lines to exclude sensors for testing
+
 #include "irSensor/irSensor.h"
+#include "ultrasonicSensor/ultrasonicSensor.h"
+#include "tofSensor/tofSensor.h"
 #
 
 #endif
