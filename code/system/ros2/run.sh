c7fdc568ae9a33878ff9d0aeb8d12f70698eb581
= <=>
2026-04-24 13:47:35 -0400

    Using AI to try and get packetized serial working

--- code/system/ros2/run.sh
+++ code/system/ros2/run.sh
@@ -6,5 +6,5 @@ docker run -it --rm \
     -e QT_X11_NO_MITSHM=1 \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v /home/ethan-reynolds/github/robot-rover-2025/code/system/ros2:/root \
-    vehicle_development_environment_ros2:foxy
+    ros-foxy-development-environment \
     bash
