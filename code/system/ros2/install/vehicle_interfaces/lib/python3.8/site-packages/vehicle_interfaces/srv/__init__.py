c673528c0f6138f9e82d53e785f612f59cd4f2ff
Ethan Reynolds <ethanreynolds451@gmail.com>
2026-05-17 09:44:54 -0400

    Fixed cmake name mismatch

--- code/system/ros2/install/vehicle_interfaces/lib/python3.8/site-packages/vehicle_interfaces/srv/__init__.py
+++
@@ -1,3 +0,0 @@
-from vehicle_interfaces.srv._get_device_status import GetDeviceStatus  # noqa: F401
-from vehicle_interfaces.srv._get_serial_port import GetSerialPort  # noqa: F401
-from vehicle_interfaces.srv._reset_serial_port import ResetSerialPort  # noqa: F401
